b1efedfd5315d422169eee4651957d061f6580f6
alicht <avilichtschein@gmail.com>
2015-12-10 09:11:57 -0500

    swapped out sqlite for postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151209022818) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
